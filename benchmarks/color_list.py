e34d9e68dd2ca5ff2fe71c30eb7549e992117d1b
jfornera <jfornera@student.ethz.ch>
2026-09-14 23:03:12 +0200

    Runtime and scaling plots

---
+++ benchmarks/color_list.py
@@ -0,0 +1,11 @@
+import matplotlib.pyplot as plt
+
+color_list = {
+    "yasmin_numpy": "#1f77b4",
+    "yasmin_cpp": "#cf6b14",
+    "yasmin_openmp": "#148714",
+    "gt4py_numpy": "#7327d6",
+    "numpy": "#1f77b4",
+    "cpp": "#cf6b14",
+    "cpp_openmp": "#148714"
+}
