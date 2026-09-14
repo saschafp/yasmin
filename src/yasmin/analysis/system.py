db6c0c213af99ae8b2ca7e832eac1e64471eefca
jfornera <jfornera@student.ethz.ch>
2026-09-14 21:21:26 +0200

    OMP_NUM_THREADS now takes precedence in setting number of threads

--- src/yasmin/analysis/system.py
+++ src/yasmin/analysis/system.py
@@ -3,6 +3,17 @@ import os
 
 def available_cores() -> int:
     try:
-        return len(os.sched_getaffinity(0))
+        affinity_cores = len(os.sched_getaffinity(0))
     except AttributeError:  # macOS / Windows
-        return os.cpu_count() or 1
+        affinity_cores = os.cpu_count() or 1
+
+    omp_num_threads = os.environ.get("OMP_NUM_THREADS")
+    if omp_num_threads is not None:
+        try:
+            omp_value = int(omp_num_threads)
+        except ValueError:
+            return affinity_cores
+        if omp_value > 0:
+            return min(omp_value, affinity_cores)
+
+    return affinity_cores
